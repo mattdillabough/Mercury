104f09edbcc13195a23fab4a8b4a75365e0befa9
junqili <junqili259@gmail.com>
2021-02-19 12:45:43 -0500

    Update AppTextInput.js

--- src/components/AppTextInput.js
+++ src/components/AppTextInput.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { TextInput, View, StyleSheet, Platform } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-//import colors from '../config/colors';
-//import defaultStyles from '../config/styles';
 
 function AppTextInput({icon, ...otherProps}) {
     return (
