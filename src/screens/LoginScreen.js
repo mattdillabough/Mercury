8a17fcab39ae48d1ef1f934d9ad083d1fce838da
junqili <junqili259@gmail.com>
2021-02-18 21:49:08 -0500

    create authstack

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function LoginScreen(props) {
+    return (
+        <View>
+            <Text>Login Screen</Text>
+        </View>
+    );
+}
+
+export default LoginScreen;
