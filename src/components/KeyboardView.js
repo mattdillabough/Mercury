b85f5284d7c389628eac3bcfd31f114e6569b2cf
junqili <junqili259@gmail.com>
2021-03-04 20:57:34 -0500

    Update KeyboardView.js

--- src/components/KeyboardView.js
+++ src/components/KeyboardView.js
@@ -5,7 +5,13 @@ function KeyboardView({ children, ...otherProps }) {
     return (
         // User's keyboard will no longer cover input fields such as on Login/Register screens.
         // Input text fields will automatically move up upon active keyboard
-        <KeyboardAvoidingView behavior={(Platform.OS === 'ios') ? "padding" : null} {...otherProps} >
+        <KeyboardAvoidingView behavior={(Platform.OS === 'ios') ? "padding" : "height"} enabled={(Platform.OS === 'ios') ? true:false} {...otherProps} 
+            keyboardVerticalOffset={
+                Platform.select({
+                    ios: () => 0, 
+                    android: () => 0
+                    })()
+                }>
             {children}
         </KeyboardAvoidingView>
     );
