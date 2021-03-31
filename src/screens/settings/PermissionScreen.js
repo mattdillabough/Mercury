cb624d27d1388b114b66ddc8f815fb2548da5104
junqili <junqili259@gmail.com>
2021-03-31 16:37:47 -0400

    Permission alert message

--- src/screens/settings/PermissionScreen.js
+++ src/screens/settings/PermissionScreen.js
@@ -1,9 +1,48 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View, Alert } from 'react-native';
 
 import { Screen } from '../../components';
+import { auth } from '../../firebase/firebase';
 
-function PermissionScreen(props) {
+
+function PermissionScreen({ navigation }) {
+
+    const [isAuthor, setIsAuthor] = useState(false);
+
+    useEffect(() => {
+        checkForAuthor();
+    })
+
+    const checkForAuthor = () => {
+        auth.currentUser.getIdTokenResult()
+        .then((idTokenResult) => {
+            if(!!idTokenResult.claims.admin){
+                // permit access to permissions
+                setIsAuthor(true);
+            } else {
+                // deny access to permissions
+                showAlert();
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    }
+
+
+    const showAlert = () => {
+        Alert.alert(
+            "Permission denied",
+            "Access not granted due to current role",
+            [
+                {
+                    text: "OK",
+                    onPress: () => navigation.navigate("Setting")
+                }
+            ]
+        )
+    }
+
     return (
         <Screen>
             <Text>Permissions</Text>
