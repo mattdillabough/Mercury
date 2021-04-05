6040dbcf216c48856afaa5d23531a85b4792d742
Eric Widjaja <ericwidjaja@pursuit.org>
2021-04-05 18:28:48 -0400

    update SettingScreen and ProfileSscreen

--- src/screens/tabs/ProfileTabScreen.js
+++ src/screens/tabs/ProfileTabScreen.js
@@ -4,6 +4,9 @@ import { createStackNavigator } from "@react-navigation/stack";
 import ProfileScreen from "../settings/ProfileScreen";
 import SettingScreen from "../settings/SettingScreen";
 import PermissionScreen from "../settings/PermissionScreen";
+import PasswordScreen from "../settings/PasswordScreen"
+// import LocationScreen from "../settings/ChangeLocationScreen";
+
 
 const Stack = createStackNavigator();
 
@@ -14,6 +17,8 @@ function ApplicationsTabScreen(props) {
       <Stack.Screen name="Profile" component={ProfileScreen} options={{headerShown: false}}/>
       <Stack.Screen name="Setting" component={SettingScreen} options={{headerShown: false}} />
       <Stack.Screen name="Permissions" component={PermissionScreen} />
+      <Stack.Screen name="Change Password" component={PasswordScreen} />
+      {/* <Stack.Screen name="Change Location" component={LocationScreen} /> */}
     </Stack.Navigator>
   );
 }
