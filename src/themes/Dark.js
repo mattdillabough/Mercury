01d6ca6e91cd7546e2dce91c2a304d99f7754026
Samuel Obe <samuel.obe@outlook.com>
2021-07-26 18:02:11 -0400

    Created dark and light theme values in themes folder

---
+++ src/themes/Dark.js
@@ -0,0 +1,10 @@
+const dark = {
+    theme: {
+      backgroundColor: 'black',
+      cardColor: "#221f20",
+      accentColor: "#FFC947", // used for buttons
+      textColor: "white"
+    }
+  }
+  
+  export default dark
---
+++ src/themes/Light.js
@@ -0,0 +1,10 @@
+const light = {
+    theme: {
+      backgroundColor: 'white',
+      cardColor: "#F5F5F5",
+      accentColor: "#FFC947", // used for buttons
+      textColor: "black"
+    }
+  }
+  
+  export default light
